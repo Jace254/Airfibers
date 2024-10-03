1a420c848467097aa79499805f9fb95c7e28a49e
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-10-03 15:48:55 +0300

    chore: init dashboard

---
+++ apps/dashboard/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    'radix-vue/nuxt',
+    '@nuxt/image',
+    'nuxt-og-image'
+  ],
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
+  ssr: true,
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
