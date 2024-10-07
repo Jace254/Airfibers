8b5d571389ff73ec53d349e0c9ea13b44ba4e360
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-10-08 02:49:46 +0300

    chore(dashboard): update navigation bar styling

--- apps/dashboard/components/navigation/NavigationItem.vue
+++ apps/dashboard/components/navigation/NavigationItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { NavItem } from '@/types'
 import { cn } from '@/utils'
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
 
 defineProps<NavItem>()
 
@@ -14,21 +15,30 @@ function navClicked() {
 <template>
   <div class="text-md relative my-[1px] rounded-md">
     <div class="block">
-      <NuxtLink
-        :to="to" draggable="false"
-        :class="cn(router.fullPath === to ? 'bg-hover' : 'bg-transparent')"
-        class="block cursor-pointer rounded-md transition-colors transition-duration-[0.15s] hover:bg-hover hover:text-foreground text-faded"
-        @click="navClicked"
-      >
-        <span class="h-[28px] flex items-center gap-1 rounded-sm pe-[2px] ps-[6px]">
-          <div v-if="icon" :class="cn(icon, 'text-lg')" />
-          <span class="text-sm">{{ title }}</span>
-        </span>
-      </NuxtLink>
+      <Tooltip>
+        <TooltipTrigger as-child>
+          <NuxtLink :to="to" draggable="false" :class="cn(router.fullPath === to ? 'bg-hover' : 'bg-transparent')"
+            class="block cursor-pointer rounded-md transition-colors transition-duration-[0.15s] hover:bg-hover hover:text-foreground text-faded"
+            @click="navClicked">
+            <span
+              class="h-[28px] flex items-center max-lg:justify-center lg:gap-1 rounded-sm px-[2px] lg:pe-[2px] lg:ps-[6px]">
+              <div v-if="icon" :class="cn(icon, 'text-lg')" />
+              <span class="text-sm hidden lg:block">{{ title }}</span>
+            </span>
+          </NuxtLink>
+        </TooltipTrigger>
+        <TooltipContent class="bg-gray-800 text-white py-1 px-1 rounded-md font-sans text-xs my-[10px] lg:hidden"
+          align="center" side="right" :side-offset="3">
+          <p>{{ title }}</p>
+        </TooltipContent>
+      </Tooltip>
+      <TooltipWrapper :content="title" align="center" side="right">
+
+      </TooltipWrapper>
     </div>
   </div>
   <template v-if="subItems">
-    <div class="anchor">
+    <div class="anchor max-lg:hidden">
       <template v-for="subItem, idx in subItems" :key="idx">
         <NavigationItem :title="subItem.title" :to="subItem.to" />
       </template>
--- apps/dashboard/components/navigation/NavigationSection.vue
+++ apps/dashboard/components/navigation/NavigationSection.vue
@@ -16,22 +16,29 @@ onMounted(() => {
 </script>
 
 <template>
-  <Collapsible v-model:open="isOpen" class="ml-[-8px]">
-    <div ref="headerEl"
-      class="mb-[1px] ml-[8px] mt-[8px] flex flex-row cursor-pointer items-center justify-between rounded-md hover:bg-hover">
-      <CollapsibleTrigger as-child>
-        <button class="h-[24px] flex grow-1 items-center self-start rounded-md px-[4px] leading-[4px]">
-          <span class="text-left text-[0.75rem] text-faded">{{ title }}</span>
-          <div
-            class="i-material-symbols-light-play-arrow-rounded text-md ml-[2px] mt-[2px] transition-transform duration-[0.15s] will-change-transform text-semifaded"
-            :class="cn(isOpen ? 'rotate-90' : 'rotate-0')" />
-        </button>
-      </CollapsibleTrigger>
-    </div>
-    <CollapsibleContent :open="isOpen" class="pos-initial h-auto overflow-hidden align-top opacity-100">
-      <div v-for="navItem, idx in navItems" :key="idx" class="ml-[8px] pb-[1px] pt-[0.05px]">
-        <NavigationItem :icon="navItem.icon" :title="navItem.title" :sub-items="navItem.subItems" :to="navItem.to" />
+  <div class="lg:block hidden">
+    <Collapsible v-model:open="isOpen" class="ml-[-8px]">
+      <div ref="headerEl"
+        class="mb-[1px] ml-[8px] mt-[8px] flex flex-row cursor-pointer items-center justify-between rounded-md hover:bg-hover">
+        <CollapsibleTrigger as-child>
+          <button class="h-[24px] flex grow-1 items-center self-start rounded-md px-[4px] leading-[4px]">
+            <span class="text-left text-[0.75rem] text-faded">{{ title }}</span>
+            <div
+              class="i-material-symbols-light-play-arrow-rounded text-md ml-[2px] mt-[2px] transition-transform duration-[0.15s] will-change-transform text-semifaded"
+              :class="cn(isOpen ? 'rotate-90' : 'rotate-0')" />
+          </button>
+        </CollapsibleTrigger>
       </div>
-    </CollapsibleContent>
-  </Collapsible>
+      <CollapsibleContent :open="isOpen" class="pos-initial h-auto overflow-hidden align-top opacity-100">
+        <div v-for="navItem, idx in navItems" :key="idx" class="ml-[8px] pb-[1px] pt-[0.05px]">
+          <NavigationItem :icon="navItem.icon" :title="navItem.title" :sub-items="navItem.subItems" :to="navItem.to" />
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
+  </div>
+  <div class="lg:hidden block pos-initial h-auto overflow-hidden align-top opacity-100 ">
+    <div v-for="navItem, idx in navItems" :key="idx" class="mx-[1px] pb-[1px] pt-[0.05px]">
+      <NavigationItem :icon="navItem.icon" :title="navItem.title" :sub-items="navItem.subItems" :to="navItem.to" />
+    </div>
+  </div>
 </template>
