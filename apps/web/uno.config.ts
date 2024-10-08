0215d249db048641ea7f842351a8dcd500528bd1
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-10-08 06:56:57 +0300

    chore: change styles

--- apps/web/uno.config.ts
+++ apps/web/uno.config.ts
@@ -12,8 +12,8 @@ import presetShadcn from './preset.shadcn'
 export default defineConfig({
   shortcuts: {
     'border-base': 'border-gray-200 dark:border-gray-800',
-    'bg-active': 'bg-gray:10',
-    'bg-faded': 'bg-gray:2',
+    'bg-active': 'bg-gray:10 dark:bg-white:10',
+    'bg-faded': 'bg-gray-100 dark:bg-gray-700',
     'bg-hover': 'dark:bg-accent:80 bg-gray:30',
     'bg-base': 'bg-background',
     'text-faded': 'text-gray-800:70 dark:text-gray-200:70',
