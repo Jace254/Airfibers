ab5ec17ac30e147e4e4c42aa2f73140148ebca8f
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-10-13 01:36:38 +0300

    chore(dashboard): fix  middleware

--- apps/dashboard/middleware/auth.global.ts
+++ apps/dashboard/middleware/auth.global.ts
@@ -1,15 +1,17 @@
 import { useSession } from "@/composables/useAuth"
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    const session = useSession()
+    try {
+        const session = useSession()
 
+        if (session.value.error && !to.fullPath.includes("/auth")) {
+            return navigateTo('/auth/sign-in')
+        }
 
-    if (session.value.error && !to.fullPath.includes("/auth")) {
+        if (session.value.data && to.fullPath.includes("/auth")) {
+            return navigateTo('/')
+        }
+    } catch (e) {
         return navigateTo('/auth/sign-in')
     }
-
-    if (session.value.data && to.fullPath.includes("/auth")) {
-        return navigateTo('/')
-    }
-
 })                                                                         
